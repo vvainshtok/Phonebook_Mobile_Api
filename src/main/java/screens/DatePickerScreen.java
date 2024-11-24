12e799ea1d4a521a08c68d3b3989e56f53de0818
vvainshtok <vvainshtok67@gmail.com>
2024-11-24 16:20:53 +0200

    date picker positive test added

--- src/main/java/screens/DatePickerScreen.java
+++ src/main/java/screens/DatePickerScreen.java
@@ -3,9 +3,12 @@ package screens;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class DatePickerScreen extends BaseScreen {
     public DatePickerScreen(AppiumDriver<AndroidElement> driver) {
@@ -14,7 +17,6 @@ public class DatePickerScreen extends BaseScreen {
 
     @FindBy(id = "com.sheygam.contactapp:id/dateBtn")
     AndroidElement btnChangeDate;
-
     @FindBy(id = "android:id/prev")
     AndroidElement btnPrevMonth;
     @FindBy(id = "android:id/next")
@@ -24,27 +26,39 @@ public class DatePickerScreen extends BaseScreen {
     @FindBy(id = "android:id/date_picker_header_year")
     AndroidElement btnPickerYear;
 
-
-
     public void typeDate(String date) {
         clickWait(btnChangeDate,2);
-        clickWait(btnPrevMonth, 2); // из-за бага
+        //clickWait(btnPrevMonth, 2); // because of the bug
 
         String[] arrayDate = date.split(" ");
         LocalDate localDate = LocalDate.now();
         if (localDate.getYear() != Integer.parseInt(arrayDate[2])) {
             btnPickerYear.click();
             driver.findElement(By.xpath("//*[@text='"+arrayDate[2]+"']")).click();
-
-
         }
+        int monthsDiff = getMonthsDiff(arrayDate[1],localDate.getMonth().toString());
+        //  "+1" and "-1" in "for" cycles added because of the bug
+        if (monthsDiff > 1)
+            for (int i = 0; i < monthsDiff-1; i++) clickWait(btnNextMonth, 2);
+        else if (monthsDiff < 1)
+            for (int i = 0; i < -monthsDiff+1; i++) clickWait(btnPrevMonth, 2);
+        else pause(3);
 
+        AndroidElement pickerDate = driver.findElement(By.xpath("//*[@content-desc='"+date+"']"));
+        clickWait(pickerDate, 4);
+        clickWait(btnOk, 3);
+    }
 
+    public int getMonthsDiff(String month1, String month2) {
+        return (Month.valueOf(month1.toUpperCase()).getValue()
+                - Month.valueOf(month2.toUpperCase()).getValue());
+    }
 
-
-        /* AndroidElement pickerDate = driver.findElement(By
-                .xpath("//*[@content-desc='"+date+"']"));
-        clickWait(pickerDate, 2);
-        */
+    public boolean validateDateSetByDatePicker(String longDate) {
+        String shortDate = LocalDate.parse(longDate, DateTimeFormatter.ofPattern("dd MMMM yyyy"))
+                .format(DateTimeFormatter.ofPattern("dd/MM/yy"));
+        WebElement dateOnScreen = driver.findElement(By.xpath("//*[@text='"
+                + shortDate + "']"));
+        return dateOnScreen.isDisplayed();
     }
 }
--- src/test/java/mobile_tests/DatePickerTests.java
+++ src/test/java/mobile_tests/DatePickerTests.java
@@ -2,23 +2,20 @@ package mobile_tests;
 
 import config.AppiumConfig;
 import dto.UserDto;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import screens.*;
-
-import java.util.Date;
 
 import static helper.PropertiesReader.getProperty;
 
 public class DatePickerTests extends AppiumConfig {
 
-
     UserDto user = UserDto.builder()
             .username(getProperty("data.properties", "email"))
             .password(getProperty("data.properties", "password"))
             .build();
-   ContactsScreen contactsScreen;
-
+    ContactsScreen contactsScreen;
 
     @BeforeMethod
     public void loginAndGoToAddNewContactScreen() {
@@ -28,13 +25,16 @@ public class DatePickerTests extends AppiumConfig {
         authenticationScreen.clickBtnLogin();
         contactsScreen = new ContactsScreen(driver);
         contactsScreen.goToDatePicker();
-
     }
 
     @Test
-    public void datePickerTest() {
+    // This test will fail because of the application bug
+    // (Wrong date is displayed after setting -- month is one less that should be,
+    // so if month is January or date like 31/10, the application crushes)
+    public void datePickerPositiveTest() {
+        String date = "02 August 2026";
         DatePickerScreen datePickerScreen = new DatePickerScreen(driver);
-        datePickerScreen.typeDate("01 November 2023");
-
+        datePickerScreen.typeDate(date);
+        Assert.assertTrue(datePickerScreen.validateDateSetByDatePicker(date));
     }
 }
